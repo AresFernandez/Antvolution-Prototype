040cd7b1fba6f5d24d11b1d65387447bd00066a4
AresFernandez <ares.fernandez@enti.cat>
2019-10-31 19:07:19 +0100

    Player movement
    player movement through nav mesh with mouse left click
    camera follows player

---
+++ Antvolution Prototype/Assets/Player_Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    Camera playerCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        playerCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            Ray ray = playerCam.ScreenPointToRay(point);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                GoTo(hit.point);
+
+            }
+        }
+    }
+
+    void GoTo(Vector3 _destination)
+    {
+        agent.destination = _destination;
+    }
+
+}

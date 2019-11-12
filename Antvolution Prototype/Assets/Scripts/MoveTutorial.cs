632683931df31300ce4b7e395ec9a562839abe4d
AresFernandez <ares.fernandez@enti.cat>
2019-11-12 11:12:10 +0100

    Onboard v1

---
+++ Antvolution Prototype/Assets/Scripts/MoveTutorial.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class MoveTutorial : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public float fadeInSpeed = 0.01f;
+    public float minimumTime = 3.0f;
+    public Image image;
+    private Color c;
+    private float startTime;
+    private bool leftClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        leftClick = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            leftClick = true;
+        }
+
+        if (Time.time - startTime >= minimumTime && leftClick)
+        {
+            c = text.color;
+            c.a -= fadeInSpeed;
+            text.color = c;
+            image.color = c;
+            if (c.a <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Antvolution Prototype/Assets/Scripts/OrderTutorial.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OrderTutorial : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI text2;
+    public TextMeshProUGUI text3;
+    public TextMeshProUGUI text4;
+    public float fadeInSpeed = 0.01f;
+    public float minimumTime = 3.0f;
+    public Image image;
+    public Image image2;
+    public Image image3;
+    public Image image4;
+    public Image image5;
+    private Color c;
+    private float startTime;
+    private bool leftClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        leftClick = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            leftClick = true;
+        }
+
+
+        //TO DO
+        if (Time.time - startTime >= minimumTime && leftClick)
+        {
+            c = text.color;
+            c.a -= fadeInSpeed;
+            text.color = c;
+            image.color = c;
+            if (c.a <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
